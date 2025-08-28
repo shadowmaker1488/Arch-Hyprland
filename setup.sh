9321c79af7367f16a38eb4e275d6b0d7312282d8
tvancml <tovanc@centrum.cz>
2025-08-28 10:05:43 +0200

    61. commit

--- setup.sh
+++ setup.sh
@@ -14,8 +14,8 @@ sudo pacman -Syu --noconfirm
 sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
 sudo pacman-key --lsign-key 3056513887B78AEB
 
-sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' --noconfirm
-sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst' --noconfirm
+sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
+sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
 
 # Adjust pacman
 sudo sed -i 's/^#Color/Color/' /etc/pacman.conf
